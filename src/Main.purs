2fa94207dd4fe191121ac6a01356e35d950bdb55
Jonathan King <jonathanlking@me.com>
2018-06-25 01:16:46 +0100

    Ace editor example

--- src/Main.purs
+++ src/Main.purs
@@ -1,43 +1,125 @@
 module Main where
 
 import Prelude
-import Data.Time.Duration (Milliseconds(..))
-import Data.Tuple (Tuple(..))
-import Control.Coroutine (await, Consumer)
+
+import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-import Control.Monad.Aff (Aff, delay)
-import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Console (CONSOLE)
+
+import Data.Maybe (Maybe(..))
+
 import Halogen as H
 import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
 import Halogen.VDom.Driver (runUI)
-import DOM (DOM)
 
-import Component (component)
-import Component as Comp
+import Ace.Types (ACE)
+import AceComponent (AceEffects, AceOutput(..), AceQuery(..), aceComponent)
 
-import Scribble.FSM
-import Scribble.Core
-import Scribble.WebSocket (WebSocket)
-import DOM.Websocket.WebSocket as WS
-import Type.Proxy (Proxy(..))
-import Data.Symbol (SProxy(SProxy))
-import Control.Monad.Aff.AVar (AVAR, AVar)
-import Control.Monad.Rec.Class (forever)
-import Control.Monad.Trans.Class (lift)
+-- | The application state, which in this case just stores the current text in
+-- | the editor.
+type State = { text :: String }
 
-import Partial.Unsafe (unsafeCrashWith)
-import Control.Monad.Error.Class (throwError)
-import Scribble.Halogen
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (log)
-import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
-import Control.Parallel
+-- | The query algebra for the app.
+data Query a
+  = ClearText a
+  | HandleAceUpdate String a
 
-main :: forall eff. Eff (HA.HalogenEffects (eff)) Unit
+-- | The slot address type for the Ace component.
+data AceSlot = AceSlot
+derive instance eqAceSlot :: Eq AceSlot
+derive instance ordAceSlot :: Ord AceSlot
+
+-- | The main UI component definition.
+ui :: forall eff. H.Component HH.HTML Query Unit Void (Aff (AceEffects eff))
+ui =
+  H.parentComponent
+    { initialState: const initialState
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+
+  initialState :: State
+  initialState = { text: "" }
+
+  render :: State -> H.ParentHTML Query AceQuery AceSlot (Aff (AceEffects eff))
+  render { text: text } =
+    HH.div_
+      [ HH.h1_
+          [ HH.text "ace editor" ]
+      , HH.div_
+          [ HH.p_
+              [ HH.button
+                  [ HE.onClick (HE.input_ ClearText) ]
+                  [ HH.text "Clear" ]
+              ]
+          ]
+      , HH.div_
+          [ HH.slot AceSlot aceComponent unit handleAceOuput ]
+      , HH.p_
+          [ HH.text ("Current text: " <> text) ]
+      ]
+
+  eval :: Query ~> H.ParentDSL State Query AceQuery AceSlot Void (Aff (AceEffects eff))
+  eval (ClearText next) = do
+    _ <- H.query AceSlot $ H.action (ChangeText "")
+    pure next
+  eval (HandleAceUpdate text next) = do
+    _ <- H.modify (_ { text = text })
+    pure next
+
+  handleAceOuput :: AceOutput -> Maybe (Query Unit)
+  handleAceOuput (TextChanged text) = Just $ H.action $ HandleAceUpdate text
+
+-- | Run the app!
+main :: Eff (HA.HalogenEffects (ace :: ACE, console :: CONSOLE)) Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  pure unit
+  runUI ui unit body
+
+-- module Main where
+-- 
+-- import Prelude
+-- import Data.Time.Duration (Milliseconds(..))
+-- import Data.Tuple (Tuple(..))
+-- import Control.Coroutine (await, Consumer)
+-- import Control.Monad.Eff (Eff)
+-- import Control.Monad.Eff.Console (CONSOLE, log)
+-- import Control.Monad.Aff (Aff, delay)
+-- import Control.Monad.Eff.Class (liftEff)
+-- import Halogen as H
+-- import Halogen.Aff as HA
+-- import Halogen.VDom.Driver (runUI)
+-- import DOM (DOM)
+-- 
+-- import Component (component)
+-- import Component as Comp
+-- 
+-- import Scribble.FSM
+-- import Scribble.Core
+-- import Scribble.WebSocket (WebSocket)
+-- import DOM.Websocket.WebSocket as WS
+-- import Type.Proxy (Proxy(..))
+-- import Data.Symbol (SProxy(SProxy))
+-- import Control.Monad.Aff.AVar (AVAR, AVar)
+-- import Control.Monad.Rec.Class (forever)
+-- import Control.Monad.Trans.Class (lift)
+-- 
+-- import Partial.Unsafe (unsafeCrashWith)
+-- import Control.Monad.Error.Class (throwError)
+-- import Scribble.Halogen
+-- import Control.Monad.Eff.Class (liftEff)
+-- import Control.Monad.Eff.Console (log)
+-- import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
+-- import Control.Parallel
+-- 
+-- main :: forall eff. Eff (HA.HalogenEffects (eff)) Unit
+-- main = HA.runHalogenAff do
+--   body <- HA.awaitBody
+--   pure unit
 --  io <- runUI component unit body
 --  io.subscribe (stooge io.query)
 --  (io.subscribe (app io.query))
